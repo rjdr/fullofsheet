965cbddaca9a02c49162f2e8726f2523cd746412
Ronald Rollins <ronaldrollins@Ronalds-iMac.local>
2015-10-24 18:26:08 -0700

    Functional image loading

--- script.js
+++ script.js
@@ -14,6 +14,25 @@ function downloadCanvas(link, filename) {
 	link.download = filename;
 }
 
+// Sets the sprites position to the next available slot
+function setSpritePosition(sprite, sprites){
+	if (sprites.length > 0){
+		var lastSprite = sprites[sprites.length - 1];
+		sprite.y = lastSprite.y + lastSprite.img.height;
+	}
+}
+
+// Refreshes the canvas every few frames
+// Sometimes there's a delay with loading images, and this should handle it
+function refreshCanvas(){
+	if (canvas){
+		var ctx = canvas.getContext("2d");
+		for (i = 0; i < sprites.length; i++){
+			ctx.drawImage(sprites[i].img, sprites[i].x, sprites[i].y);
+		}
+	}
+}
+
 $(document).ready(function(){
 	canvas = $("#ctx")[0];
 	$(".textfield").css("height", "auto");
@@ -35,24 +54,27 @@ $(document).ready(function(){
 				reader.onload = function(e){
 					img.src = e.target.result;
 
-					var sprite = [img, reader];
+					var sprite = {img:img, reader:reader, x:x, y:y};
+					setSpritePosition(sprite, sprites)
 					sprites.push(sprite);
 
 					// Draws the latest image
-					var ctx = $("#ctx")[0].getContext("2d");
+					// If there are previous images, draw it before the last
+					var ctx = canvas.getContext("2d");
 					//$('#viewarea').append("<img src=\""+img.src+"\" id=\"tester\">");	
 					if (img){
-						if (img.width > canvas.width){
-							canvas.width = img.width;
+						// Resize canvas, if necessary
+						if (sprite.x + img.width > canvas.width){
+							canvas.width = sprite.x + img.width;
 						}
-						if (img.height > canvas.height){
-							canvas.height = img.height;
+						if (sprite.y + img.height > canvas.height){
+							canvas.height = sprite.y + img.height;
 						}
 
 						ctx.fillStyle = '#f9f';
 						ctx.fillRect(0, 0, 70, 70);
-
-						ctx.drawImage(img, 0, 0);
+						// Draw all sprites, since resizing clears the canvas
+						refreshCanvas();
 					}			
 				}
 				
@@ -64,12 +86,13 @@ $(document).ready(function(){
 	
 	$("#downloadbutton").on("click", function(){
 			downloadCanvas(this, 'test.png');
-			}
-		)
+		}
+	)
 
-	var ctx = $("#ctx")[0].getContext("2d");
-	ctx.fillStyle = '#f90';
-	ctx.fillRect(0, 0, 70, 70);
+	// Refreshes the canvas occasionally for when images don't immediately display
+	setInterval(refreshCanvas, 100);
 
-}	
+	}	
 );
+
+
